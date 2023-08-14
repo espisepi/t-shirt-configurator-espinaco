e7bd5ccdde886271b213d7bfe45de21bf315e48e
espisepi <sepinaco@icloud.com>
2023-08-15 01:19:58 +0200

    improve style ColorPicker input

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,25 +1,24 @@
-import React, { useState } from 'react';
-import { useSnapshot } from 'valtio';
-import { state } from './store';
+import React, { useState } from 'react'
+import { useSnapshot } from 'valtio'
+import { state } from './store'
 
 function ColorPicker() {
-  const [selectedColor, setSelectedColor] = useState('#000000'); // Default color is black
+  const [selectedColor, setSelectedColor] = useState('#000000') // Default color is black
 
-  const snap = useSnapshot(state);
-
+  const snap = useSnapshot(state)
 
   const handleColorChange = (event) => {
-    setSelectedColor(event.target.value);
-    state.color = event.target.value;
-  };
+    setSelectedColor(event.target.value)
+    state.color = event.target.value
+  }
 
   return (
-    <div className='color-picker'>
-      <input type="color" value={selectedColor} onChange={handleColorChange} />
-      <div style={{ marginTop: '20px', width: '50px', height: '50px', backgroundColor: selectedColor }}></div>
-      <p>Selected color: {selectedColor}</p>
+    <div className="color-picker">
+      <input style={{ width: '40px', height: '40px' }} type="color" value={selectedColor} onChange={handleColorChange} />
+      {/* <div style={{ marginTop: '20px', width: '50px', height: '50px', backgroundColor: selectedColor }}></div>
+      <p>Selected color: {selectedColor}</p> */}
     </div>
-  );
+  )
 }
 
-export default ColorPicker;
+export default ColorPicker
